f18ada96b6ed95d5d8c2fd7bc1b29eb2b3f773d7
biali <ricardobiali@yahoo.com.br>
2026-01-06 22:54:52 -0300

    backend e frontend offline para o Windows

---
+++ backend/core/apps.py
@@ -0,0 +1,5 @@
+# backend/core/apps.py
+from django.apps import AppConfig
+
+class CoreConfig(AppConfig):
+    name = "core"
--- backend/server/settings.py
+++ backend/server/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'jobs',
     'corsheaders',
+    'core',
 ]
 
 CORS_ALLOWED_ORIGINS = [
